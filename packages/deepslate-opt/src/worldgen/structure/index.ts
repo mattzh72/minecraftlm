6395b02a72f5a0acc8072d9573cd54b1926894d7
mattzh72 <mattzh1314@berkeley.edu>
2026-01-07 16:26:04 -0800

    Remove packages

--- packages/deepslate-opt/src/worldgen/structure/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './StructurePlacement.js'
-export * from './StructurePoolElement.js'
-export * from './StructureSet.js'
-export * from './StructureTemplatePool.js'
-export * from './WorldgenStructure.js'
